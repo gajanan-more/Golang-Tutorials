caba00e25ea775c28a1780265b8983209987d55d
Gajanan More <gajananmore194@gmail.com>
2020-08-27 11:17:03 +0530

    Short Declaration of Variables

---
+++ go-tutorials/Short-Declaration/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// The ":=" is called as short declaration operator where you can declare a variable and assign a value to that variable.
+func main() {
+	x := 101
+	fmt.Println(x)
+	y := "Hello"
+	fmt.Println(y)
+}
