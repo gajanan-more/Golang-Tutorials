0a771c8c18561cebe11d462dba6bef7bf0176009
Gajanan More <gajananmore194@gmail.com>
2020-08-28 13:19:14 +0530

    Ninja Level 1: Exercise 1

---
+++ go-tutorials/Section 1/Ninja Level 1/exercise1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Variable Declaration using Short Declaration Operator
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// Single Print Statement. You can use any other method to Print it in single statement.
+	fmt.Println(x, y, z)
+
+	// Multiple Print Statements
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
